ea4cb302608983dae4490a8eb2c35fddfb8e262d
Andeerson <159457050+andersonmonsal@users.noreply.github.com>
2024-11-07 20:47:17 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,5 @@
 import sys
 sys.path.append("src")
 # Do not expose your Neon credentials to the browser
-PGHOST='ep-snowy-pond-a59gqbjt.us-east-2.aws.neon.tech'
-PGDATABASE='neondb'
-PGUSER='neondb_owner'
-PGPASSWORD='FRtbJLNh06of'
+# po tu base de datos
 PGPORT = '5432'  
